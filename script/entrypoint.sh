c1ff1bdc8017d787efcde1b6c9b7d1d61ed0a107
tvh25082004@gmail.com <tvh25082004@gmail.com>
2024-09-13 13:39:03 +0700

    fix

--- script/entrypoint.sh
+++ script/entrypoint.sh
@@ -1,26 +1,22 @@
 #!/bin/bash
 set -e
 
-# Kiểm tra và cài đặt các yêu cầu từ requirements.txt
 if [ -e "/opt/airflow/requirements.txt" ]; then
-  python -m pip install --upgrade pip
-  pip install --user -r /opt/airflow/requirements.txt
+  $(command python) pip install --upgrade pip
+  $(command -v pip) install --user -r requirements.txt
 fi
 
-# Khởi tạo cơ sở dữ liệu Airflow và tạo người dùng admin nếu chưa có
 if [ ! -f "/opt/airflow/airflow.db" ]; then
-  airflow db init
+  airflow db init && \
   airflow users create \
     --username admin \
     --firstname admin \
     --lastname admin \
     --role Admin \
-    --email tvh25082004@gmail.com \
+    --email admin@example.com \
     --password admin
 fi
 
-# Cập nhật cơ sở dữ liệu Airflow
-airflow db upgrade
+$(command -v airflow) db upgrade
 
-# Chạy Airflow webserver
 exec airflow webserver
